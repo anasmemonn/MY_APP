499be9c61e5a8e2a45eec3e2d38fc1417733c415
anasmemonn <143410893+anasmemonn@users.noreply.github.com>
2025-02-24 18:26:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "my-app-cache-v1";
+const urlsToCache = [
+    "/index.html",
+    "/nn.html",
+    "/ui.html",
+    "/xx.html",
+    "/manifest.json",
+    "/imagee.jpg",
+    "/isagi.jpg"
+];
+
+// Install Service Worker & Cache Files
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve Cached Files
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Service Worker & Clear Old Cache
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
